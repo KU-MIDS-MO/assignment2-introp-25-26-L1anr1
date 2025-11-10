0c9f8c3592a4876efca9d257b3be9ae3b42bd603
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2025-11-10 14:57:44 +0200

    Update breakdown_time.py

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,13 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if type(seconds) != int or seconds < 0:
+        return -1
+    if seconds == 0:
+        return {}
+    h = 0
+    m = 0
+    s = 0
+    h += int(seconds/3600)
+    m += int((seconds%3600)/60)
+    s = seconds - h*3600 - m*60
+    
+    return {3600: h, 60: m, 1: s}
